6a0199bf4506a4b0e0ed181a5239352546a528c9
Flame4Game <leo.luo0715@gmail.com>
2025-10-04 13:52:38 -0400

    added line types

--- transport/line/line.gd
+++ transport/line/line.gd
@@ -8,7 +8,9 @@ var station1 : Station
 var station2 : Station
 
 @export
-var line_type := 0
+var line_type := LineType.BUS
+
+enum LineType { BUS, METRO, BIKE }
 
 @export
 var line_width := 10.0
@@ -18,17 +20,57 @@ var line_color := Color.WHITE
 
 var distance := -1.0
 
-@export var emission_factor := 1.0
+@export var base_emission_factor := 1.0
 var traffic := 0.0
 var passengers_in_transit := []  # Array of {destination: Station, travel_time: float}
-@export var travel_speed := 50.0  # Units per second
+@export var base_travel_speed := 50.0  # Units per second
+@export var base_carbon_cost := 10.0
+
+var emission_factor := 0
+var carbon_cost := 0
+var travel_speed := 0
+
+const MODE_PROPERTIES = {
+	LineType.BUS: {
+		"emission_multiplier": 1.0,
+		"cost_multiplier": 1.0,
+		"speed_multiplier": 1.0,
+		"color": Color.RED
+	},
+	LineType.METRO: {
+		"emission_multiplier": 0.5,  # Metro is cleaner
+		"cost_multiplier": 3.0,      # But expensive to build
+		"speed_multiplier": 2.0,     # And faster
+		"color": Color.BLUE
+	},
+	LineType.BIKE: {
+		"emission_multiplier": 0.0,  # No emissions!
+		"cost_multiplier": 0.3,      # Cheap to build
+		"speed_multiplier": 0.5,     # But slower
+		"color": Color.GREEN
+	}
+}
 
 func _ready() -> void:
 	assert(station1)
 	assert(station2)
+	apply_line_mode()
 
 	station1.connected_lines.push_back(self)
 	station2.connected_lines.push_back(self)
+
+func apply_line_mode() -> void:
+	var props = MODE_PROPERTIES[line_type]
+	
+	# Apply multipliers
+	emission_factor = base_emission_factor * props.emission_multiplier
+	travel_speed = base_travel_speed * props.speed_multiplier
+	
+	# Set color based on mode
+	line_color = props.color
+	assert(distance > 0)
+	# Calculate construction cost (will be accurate after _draw calculates distance)
+	carbon_cost = distance * base_carbon_cost * props.cost_multiplier
 
 func add_passenger(destination: Station) -> void:
 	assert(distance > 0)
@@ -86,9 +128,16 @@ func _draw() -> void:
 		distance += points[i].distance_to(points[i + 1])
 		points[i] = to_local(points[i])
 	points[-1] = to_local(points[-1])
+	
+	if carbon_cost == 0.0:  # Only calculate once
+		var props = MODE_PROPERTIES[line_type]
+		carbon_cost = distance * base_carbon_cost * props.cost_multiplier
 
 	draw_polyline(points, line_color, line_width)
 	var collision_shape := ConcavePolygonShape2D.new()
 	points.insert(1, points[1])
 	collision_shape.segments = points
 	get_node("CollisionShape2D").shape = collision_shape
+
+func get_construction_cost() -> float:
+	return carbon_cost
